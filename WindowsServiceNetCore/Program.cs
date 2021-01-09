7d1f848781224e008f2f24098dd8cb19f90a8992
Janusz <xjanusz4@gmail.com>
2021-01-09 09:03:07 +0100

    Add project files.

---
+++ WindowsServiceNetCore/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using Topshelf;
+
+namespace WindowsServiceNetCore
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            HostFactory.Run(service => {
+                service.Service<Service>();
+                service.EnableServiceRecovery(recover => recover.RestartService(TimeSpan.FromSeconds(10)));
+                service.SetServiceName("WindowsServiceNetCore");
+                service.SetDescription("An example of a Windows service on Net Core");
+                service.StartAutomatically();
+            });
+        }
+    }
+}
---
+++ WindowsServiceNetCore/Service.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+using System.Timers;
+using Topshelf;
+
+namespace WindowsServiceNetCore
+{
+    public class Service : ServiceControl
+    {
+        Timer timer = new Timer();
+        public void Log(string message)
+        {
+            WriteToFile(message);
+            
+        }
+        
+        public bool Start(HostControl hostControl)
+        {
+            Log($"Start service at: {DateTime.Now}");
+
+            timer.Elapsed += new ElapsedEventHandler((sender, e) => OnElapsedTime(sender, e, $"Service recall at: {DateTime.Now}"));
+            timer.Interval = 5000;
+            timer.Enabled = true;
+
+            return true;
+        }
+
+        public bool Stop(HostControl hostControl)
+        {
+            Log($"Stop service at: {DateTime.Now}");
+            return true;
+        }
+
+        public void WriteToFile(string message)
+        {
+            string path = AppDomain.CurrentDomain.BaseDirectory + "\\Logs";
+
+            if (!Directory.Exists(path)) {
+                Directory.CreateDirectory(path);
+            }
+
+            string filePath = AppDomain.CurrentDomain.BaseDirectory + "\\Logs\\log.txt";
+            if (!File.Exists(filePath)) {
+                using(StreamWriter sw = File.CreateText(filePath)) {
+                    sw.WriteLine(message);
+                }
+            } else {
+                using (StreamWriter sw = File.AppendText(filePath)) {
+                    sw.WriteLine(message);
+                }
+            }
+        }
+
+        private void OnElapsedTime(object source, ElapsedEventArgs e, string message)
+        {
+            WriteToFile(message);
+        }
+    }
+}
